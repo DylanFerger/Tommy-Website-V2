ee832cb641ce02fb5ba217a1d8dba10844b70e4f
Fergster1299 <fergdyl@gmail.com>
2025-06-09 11:24:44 -0400

    Tommy's full website up to this point. Changes to do to make it less portfolio and more website with information.

---
+++ js/main.js
@@ -0,0 +1,8 @@
+document.getElementById('openMenu').onclick = dropMenu
+
+function dropMenu() {
+    let dropMenu = document.getElementById('dropMenu')
+    dropMenu.classList.toggle('openMenu')
+}
+
+   
