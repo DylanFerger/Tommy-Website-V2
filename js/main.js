6be38ef7ada90b4357ef440b74096270476fe344
Fergster1299 <fergdyl@gmail.com>
2025-07-23 16:59:23 -0400

    updates on nav and header

--- js/main.js
+++ js/main.js
@@ -5,4 +5,15 @@ function dropMenu() {
     dropMenu.classList.toggle('openMenu')
 }
 
-   
+let menu = document.getElementById('menu');
+let offset = menu.offsetTop;
+
+window.onscroll = function() {
+    menu.style.opacity = '0';
+
+    if (window.scrollY > offset-0) {
+        menu.classList.add("sticky");
+    } else if(window.scrollY < offset-10) {
+        menu.classList.remove("sticky");
+    }
+}  
