20b651df7fd254f7e18f329c0dfbc8ae574c9629
Fergster1299 <fergdyl@gmail.com>
2025-07-24 00:46:52 -0400

    finished menu and started sections

--- js/main.js
+++ js/main.js
@@ -1,8 +1,15 @@
-document.getElementById('openMenu').onclick = dropMenu
+const button1 = document.getElementById('openMenu1');
+const button2 = document.getElementById('openMenu2');
+
+button1.addEventListener('click', dropMenu);
+button2.addEventListener('click', dropMenu);
+
+let menuButton = document.getElementById('openMenu')
 
 function dropMenu() {
     let dropMenu = document.getElementById('dropMenu')
     dropMenu.classList.toggle('openMenu')
+    console.log('hi')
 }
 
 let menu = document.getElementById('menu');
@@ -15,5 +22,6 @@ window.onscroll = function() {
         menu.classList.add("sticky");
     } else if(window.scrollY < offset-10) {
         menu.classList.remove("sticky");
+        menu.style.opacity = '1';
     }
 }  
